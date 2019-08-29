6b13e554b6ca7be780d7391c4e38febb19aa0a2e
Gr8-m8 <martinn.lindbladd@gmail.com>
2019-08-29 11:50:40 +0200

    Grunder

---
+++ Gymnasiearbete/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gymnasiearbete
+{
+    class Cell
+    {
+        CellManager CM;
+
+        int energy = 0;
+        int speed = 0;
+        int size = 0;
+        int perception = 0;
+
+        int energyRequirement = 0;
+
+        public Cell(CellManager setCellManager)
+        {
+            CM = setCellManager;
+        }
+
+        void Reproduce()
+        {
+            if (energy > energyRequirement)
+            {
+                energy -= energyRequirement;
+                for (int i = 0; i < energy/energyRequirement; i++)
+
+                CM.cells.Add(new Cell(CM));
+            }
+        }
+    }
+}
---
+++ Gymnasiearbete/CellManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gymnasiearbete
+{
+    class CellManager
+    {
+        public List<Cell> cells = new List<Cell>();
+    }
+}
