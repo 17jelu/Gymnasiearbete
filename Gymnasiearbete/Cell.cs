247a8a61aead6701466f752b8a4ae8c36c2a8269
Gr8-m8 <martinn.lindbladd@gmail.com>
2019-10-01 15:00:46 +0200

    *Cell(basfunktion), *CellManager(basfunktion)

--- Gymnasiearbete/Cell.cs
+++ Gymnasiearbete/Cell.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Gymnasiearbete
 {
+    /// <summary>
+    /// Grund Cell klassen
+    /// </summary>
     class Cell
     {
         CellManager CM;
 
+        Vector2 position = Vector2.Zero;
+        double rotation = 0;
+
+        int curiosity = 0;
         int energy = 0;
         int speed = 0;
         int size = 0;
         int perception = 0;
+
+        int preformancepoints = 0;
 
         int energyRequirement = 0;
 
@@ -30,6 +40,26 @@ namespace Gymnasiearbete
 
                 CM.cells.Add(new Cell(CM));
             }
+        }
+
+        Vector2 Forward()
+        {
+            return new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
+        }
+
+
+
+        public void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Retunerar data för att rita. x-pos, y-pos, size, rotation
+        /// </summary>
+        public float[] DrawData
+        {
+            get { return new float[4] { position.X, position.Y, size, (float)rotation }; }
         }
     }
 }
--- Gymnasiearbete/CellManager.cs
+++ Gymnasiearbete/CellManager.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Gymnasiearbete
 {
+    /// <summary>
+    /// Kontrollerar Alla Celler
+    /// </summary>
     class CellManager
     {
         public List<Cell> cells = new List<Cell>();
+
+        public void Update()
+        {
+            foreach (Cell c in cells)
+            {
+                c.Update();
+            }
+        }
     }
 }
