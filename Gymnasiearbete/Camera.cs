7d05e5092abd08ccbf0ac3728a82c9f5a5e224e9
Jesper Lundgren <17jelu@stockholmscience.se>
2019-10-01 14:32:09 +0200

    +Camera.cs

---
+++ Gymnasiearbete/Camera.cs
@@ -0,0 +1,17 @@
+﻿namespace Gymnasiearbete
+{
+    class Camera
+    {
+        private int x, y;
+        public int X // Temporary
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y // Temporary
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
