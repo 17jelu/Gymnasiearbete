cf3ccccb53cad17af456dae850cd58e6af5f2579
Gr8-m8 <martinn.lindbladd@gmail.com>
2019-11-05 10:22:40 +0100

    +Memory()

---
+++ Gymnasiearbete/Memory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gymnasiearbete
+{
+    class Memory
+    {
+        static char z = '|';
+
+        string situation;
+        public string Situation
+        {
+            get
+            {
+                return situation;
+            }
+        }
+        int points;
+        public int Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                points = value;
+            }
+        }
+        string decision;
+        public string Desicion
+        {
+            get
+            {
+                return decision;
+            }
+            set
+            {
+                decision = value;
+            }
+        }
+
+        public Memory(string situationSet, string pointsSet, string decisionSet)
+        {
+            situation = situationSet;
+            points = int.Parse(pointsSet);
+            decision = decisionSet;
+        }
+
+        public new string ToString()
+        {
+            return situation + z + points + z + decision;
+        }
+    }
+}
