779768da2e3e75f3c3a398eb570992a57184a1dd
Jesper Lundgren <17jelu@stockholmscience.se>
2019-11-07 13:28:39 +0100

    +Render(Supposed to be handling rendering)

---
+++ Gymnasiearbete/Render.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gymnasiearbete
+{
+    class Render
+    {
+        public static void Draw()
+        {
+            
+        }
+    }
+}
