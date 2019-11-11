d505ae98bb7f60c510422d3afc90a6c592a89a50
Gr8-m8 <martinn.lindbladd@gmail.com>
2019-11-11 12:34:12 +0100

    -tock

--- Gymnasiearbete/Game1.cs
+++ Gymnasiearbete/Game1.cs
@@ -85,8 +85,6 @@ namespace Gymnasiearbete
             // TODO: Unload any non ContentManager content here
         }
 
-        float tick = 0;
-
         /// <summary>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
