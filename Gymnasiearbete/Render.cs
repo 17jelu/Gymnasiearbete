96a6cf765690b5269fda5fd4ddc42499cee44819
Jesper Lundgren <17jelu@stockholmscience.se>
2019-11-19 15:02:58 +0100

    Revert "*Render(Fixed grid)"
    This reverts commit 349bc2430ee3e40f8903dc75b08c5ecb3299654a.

--- Gymnasiearbete/Render.cs
+++ Gymnasiearbete/Render.cs
@@ -17,53 +17,36 @@ namespace Gymnasiearbete
         }
         public static void Draw(CellManager CM, GraphicsDevice GraphicsDevice)
         {
-            byte
-                gridWidth = (byte)(SGScreen.Area.Width / (CellManager.SectorSize * Camera.Zoom)),
-                gridHeight = (byte)(SGScreen.Area.Height / (CellManager.SectorSize * Camera.Zoom));
-            float
-                offset;
+            byte grid = (byte)(SGScreen.Area.Width / ( CellManager.SectorSize * Camera.Zoom));
+            grid += 1;
 
-            // Drawing the Grid (vertical)
-            for (int y = -(gridHeight >> 1) - 1; y < (gridHeight << 1); y++)
+            // Drawing the Grid
+            for (int i = 0; i < grid; i++)
             {
-                offset =
-                    y * CellManager.SectorSize * Camera.Zoom
-                    - (Camera.Position.Y % CellManager.SectorSize * Camera.Zoom)
-                    + (SGScreen.Area.Height >> 1);
-
                 gridLine.SetLine(
-                    new Vector2(0, offset),
-                    new Vector2(SGScreen.Area.Width, offset),
-                    false
-                );
-                gridLine.Render(GraphicsDevice);
-            }
-            // Drawing the Grid (horizontal)
-            for (int x = -(gridWidth >> 1); x < (gridWidth << 1); x++)
-            {
-                offset =
-                    x * CellManager.SectorSize * Camera.Zoom
-                    - (Camera.Position.X % CellManager.SectorSize * Camera.Zoom)
-                    + (SGScreen.Area.Width >> 1);
+                    new Vector2(
+                        i * CellManager.SectorSize * Camera.Zoom
+                        - Camera.Position.X,
 
-                gridLine.SetLine(
-                    new Vector2(offset, 0),
-                    new Vector2(offset, SGScreen.Area.Height),
+                        0
+                    ),
+                    new Vector2(
+                        i * CellManager.SectorSize * Camera.Zoom
+                        - Camera.Position.X,
+
+                        SGScreen.Area.Height
+                    ),
                     false
                 );
                 gridLine.Render(GraphicsDevice);
             }
 
             // Drawing content inside of Chunks
-            // TODO: add offset to points
-            for (int y = 0; y < gridHeight; y++)
+            for (int y = 0; y < 10; y++)
             {
-                for (int x = 0; x < gridWidth; x++)
+                for (int x = 0; x < grid; x++)
                 {
-                    Point p = new Point(
-                        x,
-                        y
-                    );
+                    Point p = new Point(x, y);
                     if (CM.Sectors.ContainsKey(p))
                     {
                         SectorContent sector = CM.Sectors[p];
@@ -95,28 +78,6 @@ namespace Gymnasiearbete
                             );
                             line.Render(GraphicsDevice);
                         }
-                    }
-                    else
-                    {
-                        line.SetLine(
-                            new Vector2(
-                                x * CellManager.SectorSize,
-                                y * CellManager.SectorSize),
-                            new Vector2(
-                                x * CellManager.SectorSize + CellManager.SectorSize,
-                                y * CellManager.SectorSize + CellManager.SectorSize
-                            ));
-                        line.Render(GraphicsDevice);
-
-                        line.SetLine(
-                            new Vector2(
-                                x * CellManager.SectorSize + CellManager.SectorSize,
-                                y * CellManager.SectorSize),
-                            new Vector2(
-                                x * CellManager.SectorSize,
-                                y * CellManager.SectorSize + CellManager.SectorSize
-                            ));
-                        line.Render(GraphicsDevice);
                     }
                 }
             }
