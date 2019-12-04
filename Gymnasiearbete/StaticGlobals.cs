66f6e1b6f304c5457776e10d38577e645d206a6f
Jesper Lundgren <17jelu@stockholmscience.se>
2019-12-04 15:12:02 +0100

    *StaticGlobals.Family(Changed families to Dictionary from List, Changed colors, +AddMember(family) & +KillMember(family))

--- Gymnasiearbete/StaticGlobals.cs
+++ Gymnasiearbete/StaticGlobals.cs
@@ -37,16 +37,16 @@ Consider not using it as it is at high risk of getting removed.";
         }
         internal class sgFamily
         {
-            List<string> families = new List<string>();
+            Dictionary<string, int> families = new Dictionary<string, int>();
             #region NameList
             string[] nl_color = new string[]
             {
                     "Red",
                     "Green",
                     "Blue",
-                    "Yellow",
-                    "Black",
-                    "White"
+                    "Cyan",
+                    "Magenta",
+                    "Yellow"
             };
             string[] nl_adjective = new string[]
             {
@@ -124,9 +124,27 @@ Consider not using it as it is at high risk of getting removed.";
                     {
                         name = GenerateRandomName();
                     }
-                    while (families.Contains(name));
+                    while (families.ContainsKey(name));
+
+                    families.Add(name, 1);
 
                     return name;
+                }
+            }
+
+            public void KillMember(string family)
+            {
+                if (families.ContainsKey(family))
+                {
+                    families[family] -= 1;
+                }
+            }
+
+            public void AddMember(string family)
+            {
+                if (families.ContainsKey(family))
+                {
+                    families[family] += 1;
                 }
             }
         }
